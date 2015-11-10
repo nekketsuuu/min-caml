9952e185c91db310c17337baf4ef6aaa1e6d3605
nekketsuuu <nekketsuuu@users.noreply.github.com>
2015-11-10 10:19:33 +0900

    Fix a bug on read_int (cartelet) read_int couldn't read successive spaces

---
+++ test/read.ml
@@ -0,0 +1,31 @@
+let in1 = read_int () in
+let in2 = read_int () in
+let in3 = read_int () in
+let in4 = read_float () in
+let in5 = read_float () in
+let in6 = read_float () in
+let in7 = read_float () in
+let in8 = read_int () in
+let in9 = read_int () in
+let in10 = read_int () in
+print_int in1;
+print_newline ();
+print_int in2;
+print_newline ();
+print_int in3;
+print_newline ();
+print_int (int_of_float in4);
+print_newline ();
+print_int (int_of_float in5);
+print_newline ();
+print_int (int_of_float in6);
+print_newline ();
+print_int (int_of_float in7);
+print_newline ();
+print_int in8;
+print_newline ();
+print_int in9;
+print_newline ();
+print_int in10;
+print_newline ();
+
